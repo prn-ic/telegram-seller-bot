9d240a3e3a7b8c0a0d6e97b310eb83f3ee3f724e
prn-ic <domarlaid2@mail.ru>
2024-08-11 18:52:09 +0500

    feature(6): исправил код

--- src/TelegramSellerBot.TelegramBot/Program.cs
+++ src/TelegramSellerBot.TelegramBot/Program.cs
@@ -1,7 +1,16 @@
+using TelegramSellerBot.Persistense;
 using TelegramSellerBot.TelegramBot;
 
+IConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+configurationBuilder
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .AddEnvironmentVariables();
+
+IConfiguration configuration = configurationBuilder.Build();
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
+builder.Services.AddNpgsqlPersistense(configuration, "TelegramSellerBot.TelegramBot");
 
 var host = builder.Build();
 host.Run();
